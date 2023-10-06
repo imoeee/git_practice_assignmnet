9e03e7af1e331debc2741165dee5235036e59423
imoeee <imrankhan78626yobro@gmail.com>
2023-10-06 08:10:12 +0530

    problem solved

--- prime.js
+++ prime.js
@@ -4,13 +4,13 @@ let num = 13;
 let flag = 'true';
 
 for(let i=2;i<num;i++){
-    if(num % i !== 0){
+    if(num % i === 0){
         flag = 'false';
         break
     }
 }
 if(flag == 'true'){
-    console.log('prime');
+    console.log('prime number');
 }else{
-    console.log('Not prime');
+    console.log('Not prime number');
 }
