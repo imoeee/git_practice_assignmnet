b985f467862755b753d538e361784e6880d674d3
imoeee <imrankhan78626yobro@gmail.com>
2023-10-06 07:25:49 +0530

    prime check

---
+++ prime.js
@@ -0,0 +1,16 @@
+//  Prime Numbers
+
+let num = 13;
+let flag = 'true';
+
+for(let i=2;i<num;i++){
+    if(num % i !== 0){
+        flag = 'false';
+        break
+    }
+}
+if(flag == 'true'){
+    console.log('prime');
+}else{
+    console.log('Not prime');
+}
